b3248290fe66d404ea7ce83d7b27924d4fde6dcc
Karina <129091450+karina725@users.noreply.github.com>
2023-03-30 11:50:27 -0600

    Add files via upload
    Signed-off-by: Karina  <129091450+karina725@users.noreply.github.com>

---
+++ ESCULTURAS.php
@@ -0,0 +1,167 @@
+<html>
+    <head>
+        <title>INDECORA</title>
+        <meta charset="UTF-8"/>
+        <meta name="description" content=""/>
+        <meta name="author" content="karina guadalupe barragan jara"/>
+    <script type="text/javascript" src="https://gc.kis.v2.scr.kaspersky-labs.com/FD126C42-EBFA-4E12-B309-BB3FDD723AC1/main.js?attr=tQMLENueorX5rq5dS1mdZQJx-sbl2F7z2udnmBJlFHOUn0uxObBoso7nJWAuTOIsQ2-nHOiLERq5CYBkNg2u5WMC3AbqmiaTplBZH9rzQJSD-8Mq2kBSd0pu0HZX1e82un4g1eN1wyKdc9bTM3kWoHWXjzMJj58EW0Gy1Y0IjuOyBZqawSUVWCQsUoFLgeo3_uKgI8mdVGwR5Ftd9NAUUm5IMUvCEal06p6dv1Kdez4lcJc72HeqJ55QrKf0qjvaB3HLiS05YcJb0l3I_rVFJ96b-08-HkGvqbueA6F1e6-qaIB4SdZsnhXVT8WlPqTK3SsjN96mHeT7LiG9lFFCn1hBVwWYROyjC3NjKjd0D4KPb7FOkNtAO1RgDyaqzJl6B5urGt-EreGKPHIdpA65A-_k036hfEO1-aE1Yfw4DEuv1Y4kjzxkxY38mfNyYQkIgm8kRgCTjMXOv9Hvq_JJyUsyi0T2cCnozgIUo7_ZaAUqXYsKNLEQ4CZ_GDJAP3AsNbCtEdn6bsPo8xRskOtCh_hT5QQZJeu0j83KhFKiXzyG6rDECoKONWqP7kdK7GmiE_NbZ2FQViBntFIXmHvCCBK5VQesW8XoeUHtAnC9iULzEelTHVFdZ-oen5k9-MjlnRUUr6nrdY4iH9bP7wziPuCkKV2jgu6omhPJ08Vkw8Flp2zfMhN23f0VNlYn05ypwkUXKUrZ7Dn1SHszIjpFs4rlvUE4NfgzPrK-p5IPq4RYLzjozGS4hXpS8yuwa-_PDEhKTC9kcRls6oTHhxK5bcpfVbVb8b0BcWFdyQTQJAWTx54gunUKZOUc85kskYcwS1nqO_llwjjjmlcyLZAH77OSQb1HGxSiT721Uyeo-mM80K6yvdItP6pQahcLiCC8rY1-WhgfxzYuUIQu7z2HvwRC8u4E61IoyvSJnjkLHhd30NPTU9rP3sTydEoZEOkNgd1ycqA5zrDZIaNJVP32v9xEcK1qyOZL2fSqNFjTFxAPAnujGiroH-827-__Rk7LcvQliISP9YfD4f4qV3Ykvv2x6WIxwBjPFlPoTPyn-SpIMiUreGhAPuAIuDUq8LeUjd1PT8BKC36FJ3rEX0zZykwNjBmJ48ZaGR147UI6DcUZowrBh-rqISm507Vnn7mSNF71fZ0AE0RrKCb7XYh8Vee7uf0W75q75aGFyuFtrnl9cSLHXGZoM0b6OzRHSMgwleZaqgjY6p_KAGMm5PrdFMznE2uRlDc0N_BefpzV_xUOPNZy_TuHEHmwbeE7-FgVazJ1x3-VagA15PKxQyLZKaQQdh6o-K09WN95939tFmN9OTbuWOjlqu133oTRk7r90TfaItn93t0sK3BZ0eW8h8AhAf53H7cbM9rxE22-CEzp3yoXQrYOVqBInc4reHkXb6QX5F1ilR_Kdtpswa7OkVkNIlaN8nzHCTHNh8fg7z1hxcp-WbD3dN4xrDWPBXZVS1c1MMLtHW9omAzCEs3DE4GgGUeINix6yINk0bZptUsftPZul3H89g37LPGCn4OPJ_ZF06TDkHe53-S7TiXkP2zQ-rgUEFOQVE7byncO8n5fTKSliCWc8O04Y9CDjsTmA-Ss4Ll2aNDkuCD_0Cofqg" charset="UTF-8"></script></head>
+    <link href="https://fonts.googleapis.com/css2?family=Shalimar&display=swap" rel="stylesheet">
+
+    <body>
+        <link type="text/css" rel="stylesheet" href="estilo/estilo1.css">
+    <div class="menuusuario">
+            <img src="imagenes/imagen3.png" width="50px" height="50px" HSPACE="0"  aling ="right" aling= "justify">BLOG&nbsp&nbsp&nbsp
+            <img src="imagenes/imagen4.png" width="50px" height="40px" HSPACE="0"  aling ="right" aling= "justify"  >CUENTA&nbsp&nbsp&nbsp&nbsp
+            <img src="imagenes/imagen5.png" width="60px" height="40px" HSPACE="0"  aling ="right" aling= "justify">UBICACION&nbsp&nbsp&nbsp
+            <img src="imagenes/imagen6.png" width="70px" height="40px" HSPACE="0"  aling ="right" aling= "justify">33-24-93-09-55
+    </div>
+    <div class="titulo">
+        <img  src="imagenes/imagen1.png"width="300px" height="200px" />
+        <div class="titulo-texto">Indecora</div>
+        <div class="titulo-texto1">UNA SOLUCION PARA TODOS</div> 
+        <img class= img src="imagenes/imagen2.png" width="150px"height="100"/>
+    </div>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+     <menu>
+        <li><a href="index.php">Inicio</a></li>
+        <li><a href="COJINES.php">Cojines</a></li>
+        <li><a href="FLOREROS.php">Floreros</a></li>
+        <li><a href="LAMPARAS.php">Lamparas</a></li>
+        <li><a href="TAPETES.php">Tapetes</a></li>
+        <li><a href="CUADROS.php">Cuadros</a></li>
+        <li><a href="ESCULTURAS.php">Esculturas</a></li>
+        <li><a href="REGALOS.php">Regalos</a></li>
+        <li><a href="OTROS.php">Otros</a></li>
+    </menu>
+    <br>
+    <br>
+    <br>
+    <br>
+    <div class="pagina">Esculturas</div>
+    <div class="pagina1">1 2 3 4 </div>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <div class="pagina2">Inicio > Esculturas  </div>
+    
+    <div id="contenedor_principal">
+        <!--Primera Imagen-->
+        <div class="contenedor_imagen_texto">
+            <div class="imagen"><img src="imagenes/imagen68.jpg" width="300px" height="300px"></div>
+            <div class="texto">PENSANTES</div>
+            <div class="texto1">Juego de 3 esculturas decorativas<br>de 45,7 x 45,7 cm ,<br>ideal para decorar tu sala.</div>
+            <div class="texto2">Precio $ 950.00</div>
+        </div>
+
+        <!--Segunda Imagen-->
+        <div class="contenedor_imagen_texto">
+            <div class="imagen"><img src="imagenes/imagen69.jpg" width="300px" height="300px"></div>
+            <div class="texto">BUDA</div>
+            <div class="texto1">Escultura de buda <br>material de acero</div><br>
+            <div class="texto2">Precio $ 650.00</div>
+        </div>
+
+        <!--tercera Imagen-->
+        <div class="contenedor_imagen_texto">
+            <div class="imagen"><img src="imagenes/imagen70.jpg" width="300px" height="300px"></div>
+            <div class="texto">DUO DE MANZANAS</div>
+            <div class="texto1">Manzanas decorativas <br>colores plata y dorado <br>material de aluminio</div>
+            <div class="texto2">Precio $ 750.00</div>
+        </div>
+    <br>
+    <br>
+    <br>
+    <br>
+        <!--cuarta Imagen-->
+        <div class="contenedor_imagen_texto">
+            <div class="imagen"><img src="imagenes/imagen71.jpg" width="300px" height="300px"></div>
+            <div class="texto">ROSTRO</div>
+            <div class="texto1">Buesto de mujer <br>con una pareja de enamorados <br>entrelazandos</div>
+            <div class="texto2">Precio $ 900.00</div>
+        </div>
+    
+        <!--quinta Imagen-->
+        <div class="contenedor_imagen_texto">
+            <div class="imagen"><img src="imagenes/imagen72.jpg" width="300px" height="300px"></div>
+            <div class="texto">PERRITO</div>
+            <div class="texto1">Llavero en forma de <br>perrito, con tonos blancoy dorado<br>altura de 50cm</div>
+            <div class="texto2">Precio $ 800.00</div>
+        </div>
+    
+        <!--sexta Imagen-->
+        <div class="contenedor_imagen_texto">
+            <div class="imagen"><img src="imagenes/imagen73.jpg" width="300px" height="300px"></div>
+            <div class="texto">BUSTO DE NIÑA CON MARIPOSA</div>
+            <div class="texto1">Hemoso busto de niña <br>con mariposa en tonalidad blanca<br>y dorada, con un toque de rosa.</div>
+            <div class="texto2">Precio $ 750.00</div>
+        </div>
+    <br>
+    <br>
+    <br>
+    <br>
+        <!--septima Imagen-->
+        <div class="contenedor_imagen_texto">
+            <div class="imagen"><img src="imagenes/imagen74.jpg" width="300px" height="300px"></div>
+            <div class="texto">ESCULTURA ABSTRACTA</div>
+            <div class="texto1">Cojín decorativo de 45,7 x 45,7<br>cm para sofá, terciopelo, varios<br>colores recámara, sofá, etc</div>
+            <div class="texto2">Precio $ 999.00</div>
+        </div>
+        
+        <!--octava Imagen-->
+        <div class="contenedor_imagen_texto">
+            <div class="imagen"><img src="imagenes/imagen75.jpg" width="300px" height="300px"></div>
+            <div class="texto">PAREJA DE CONEJOS</div>
+            <div class="texto1">Pareja de conejos<br>de 45,7 x 45,7 cm ,<br>en tonalidades blancas.</div>
+            <div class="texto2">Precio $ 990.00</div>
+        </div>
+    
+        <!--novena Imagen-->
+        <div class="contenedor_imagen_texto">
+            <div class="imagen"><img src="imagenes/imagen76.jpg" width="300px" height="300px"></div>
+            <div class="texto">PLUMAS</div>
+            <div class="texto1">Juego de plimas ideal para<br>separador de libros.<br>Tamaño 45cm</div>
+            <div class="texto2">Precio $ 870.00</div>
+        </div> 
+    </div>       
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <div class="barra">
+        <p>
+            <br>
+           Nombre: Karina Guadalupe Barragán Jara<br>Codigo: 304180712 
+           <br>Nombre Materia: Conceptualización de servicios en la nube
+           <br>Correo:<a href="mailto:kbarragan725@gmail.com">kbarragan725@gmail.com</a>
+            <br>
+            <br>
+        </p>
+    </div>   
+
+    <p>
+    <p>
+        Autor: Karina Guadalupe Barragán Jara <br> Correo:  <a href="mailto:kbarragan725@gmail.com"> kbarragan725@gmail.com</a>
+    </p>
+    </body>
+</html>
